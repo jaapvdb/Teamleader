8bdb0ae2c7730ab62cddc2359815513a3df26688
Jaap <jaap1@me.com>
2022-03-04 08:59:44 +0100

    upload setup

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
 setup(
-    name="Teamleader",
-    packages=["Teamleader"],
-    version="1.0.1",
+    name="teamleader",
+    packages=["teamleader"],
+    version="1.0.2",
     license="MIT",
     description="A layer on top of the Teamleader v2 API",
     author="Jaap",
